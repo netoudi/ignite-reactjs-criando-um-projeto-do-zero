8d1730aa80b50288bbaf484727f353a32c649bd2
netoudi <netoudi@rocketmail.com>
2021-06-08 11:53:57 -0300

    Add structure and styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import Link from 'next/link';
 import { GetStaticProps } from 'next';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -24,9 +26,33 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      {[1, 2, 3].map(post => {
+        return (
+          <div key={post} className={styles.listItem}>
+            <Link href="/post/xpto">
+              <a>
+                <h2 className={styles.heading}>Como utilizar Hooks</h2>
+              </a>
+            </Link>
+            <span className={styles.subtitle}>
+              Pensando em sincronização em vez de ciclos de vida.
+            </span>
+            <div className={styles.info}>
+              <FiCalendar size={20} /> 15 Mar 2021
+              <FiUser size={20} /> Joseph Oliveira
+            </div>
+          </div>
+        );
+      })}
+      <button type="button" className={styles.buttonShowMore}>
+        Carregar mais posts
+      </button>
+    </div>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,6 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -26,9 +27,84 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post() {
+  return (
+    <section>
+      <div className={styles.banner}>
+        <img src="/images/banner.jpg" alt="Post title" />
+      </div>
+      <div className={styles.container}>
+        <div className={styles.listItem}>
+          <a href="/post/xpto">
+            <h1 className={styles.heading}>Como utilizar Hooks</h1>
+          </a>
+          <span className={styles.subtitle}>
+            Pensando em sincronização em vez de ciclos de vida.
+          </span>
+          <div className={styles.info}>
+            <div className={styles.infoItem}>
+              <FiCalendar size={20} /> 15 Mar 2021
+            </div>
+            <div className={styles.infoItem}>
+              <FiUser size={20} /> Joseph Oliveira
+            </div>
+            <div className={styles.infoItem}>
+              <FiClock size={20} /> 4 min
+            </div>
+          </div>
+        </div>
+        <div className={styles.content}>
+          <h2>Lorem ipsum dolor.</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat in
+            maxime omnis recusandae saepe vel! Autem consequatur eveniet hic
+            illo labore magnam minus nemo, nobis nulla obcaecati odit
+            praesentium quisquam, quo, recusandae reprehenderit tempora
+            voluptates! Adipisci aut blanditiis exercitationem modi quasi quo
+            tempora veniam voluptates. Earum enim excepturi ipsa ut.
+          </p>
+          <h2>Lorem ipsum dolor.</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dignissimos harum odio quam quod! Beatae deleniti dolores dolorum
+            error et, ex facilis magni, nesciunt odit pariatur ratione
+            reiciendis rem sapiente similique. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Fugiat in maxime omnis recusandae
+            saepe vel! Autem consequatur eveniet hic illo labore magnam minus
+            nemo, nobis nulla obcaecati odit praesentium quisquam, quo,
+            recusandae reprehenderit tempora voluptates! Adipisci aut blanditiis
+            exercitationem modi quasi quo tempora veniam voluptates. Earum enim
+            excepturi <a href="/">ipsa ut</a>.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dignissimos harum odio quam quod! Beatae deleniti dolores dolorum
+            error et, ex facilis magni, nesciunt odit pariatur ratione
+            reiciendis rem sapiente similique. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Fugiat in maxime omnis recusandae
+            saepe vel! Autem consequatur eveniet hic illo labore magnam minus
+            nemo, nobis nulla obcaecati odit praesentium quisquam, quo,
+            recusandae reprehenderit tempora voluptates! Adipisci aut blanditiis
+            exercitationem modi quasi quo tempora veniam voluptates. Earum enim
+            excepturi ipsa ut.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dignissimos harum odio quam quod! Beatae deleniti dolores dolorum
+            error et, ex facilis magni, nesciunt odit pariatur ratione
+            reiciendis rem sapiente similique. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Fugiat in maxime omnis recusandae
+            saepe vel! Autem consequatur eveniet hic illo labore magnam minus
+            nemo, nobis nulla obcaecati odit praesentium quisquam, quo,
+            recusandae reprehenderit tempora voluptates! Adipisci aut blanditiis
+            exercitationem modi quasi quo tempora veniam voluptates. Earum enim
+            excepturi ipsa ut.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
 
 // export const getStaticPaths = async () => {
 //   const prismic = getPrismicClient();
